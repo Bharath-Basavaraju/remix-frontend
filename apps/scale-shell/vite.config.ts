c789408f2e575d5052be4546e51f042aa33d96c4
Bharath-Basavaraju <bharath88.b@gmail.com>
2025-08-12 15:45:32 +0530

    chore: remove shared federated deps

--- apps/scale-shell/vite.config.ts
+++ apps/scale-shell/vite.config.ts
@@ -13,7 +13,6 @@ export default {
         colleagueMenu: '/remotes/colleague-menu/remoteEntry.js',
         notification: '/remotes/notification/remoteEntry.js',
       },
-      shared: ['xstate', 'zustand'],
     }),
   ],
   optimizeDeps: {
