3a0a912f63061507ce3ceb63397bb4ddc3a9d17a
Bharath-Basavaraju <bharath88.b@gmail.com>
2025-08-12 16:38:57 +0530

    chore: update navigation machine config

--- apps/scale-shell/src/navigationMachine.ts
+++ apps/scale-shell/src/navigationMachine.ts
@@ -1,18 +1,21 @@
 import { createMachine } from 'xstate';
 
-export const navigationMachine = createMachine({
-  id: 'navigation',
-  initial: 'shell',
-  states: {
-    shell: {
-      on: {
-        PRODUCE: 'produceScale',
-        COLLEAGUE: 'colleagueMenu',
-        NOTIFICATION: 'notification'
-      }
-    },
-    produceScale: { on: { SHELL: 'shell' } },
-    colleagueMenu: { on: { SHELL: 'shell' } },
-    notification: { on: { SHELL: 'shell' } }
+export const navigationMachine = createMachine(
+  {
+    id: 'navigation',
+    predictableActionArguments: true,
+    initial: 'shell',
+    states: {
+      shell: {
+        on: {
+          PRODUCE: 'produceScale',
+          COLLEAGUE: 'colleagueMenu',
+          NOTIFICATION: 'notification'
+        }
+      },
+      produceScale: { on: { SHELL: 'shell' } },
+      colleagueMenu: { on: { SHELL: 'shell' } },
+      notification: { on: { SHELL: 'shell' } }
+    }
   }
-});
+);
